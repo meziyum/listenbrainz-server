da62012affde5ae38c6dbf0547e5a11297f8c014
Kartik Ohri <kartikohri13@gmail.com>
2022-07-29 16:28:21 +0530

    Try adding user listens session query

--- listenbrainz/labs_api/labs/main.py
+++ listenbrainz/labs_api/labs/main.py
@@ -9,7 +9,9 @@ from listenbrainz.labs_api.labs.api.mbid_mapping import MBIDMappingQuery
 from listenbrainz.labs_api.labs.api.explain_mbid_mapping import ExplainMBIDMappingQuery
 from listenbrainz.labs_api.labs.api.recording_search import RecordingSearchQuery
 from listenbrainz.labs_api.labs.api.artist_credit_recording_lookup import ArtistCreditRecordingLookupQuery
+from listenbrainz.labs_api.labs.api.user_listen_sessions import UserListensSessionQuery
 from listenbrainz.webserver import load_config
+from listenbrainz.webserver.timescale_connection import init_timescale_connection
 
 register_query(ArtistCountryFromArtistMBIDQuery())
 register_query(ArtistCreditIdFromArtistMBIDQuery())
@@ -19,7 +21,9 @@ register_query(MBIDMappingQuery())
 register_query(ExplainMBIDMappingQuery())
 register_query(RecordingSearchQuery())
 register_query(ArtistCreditRecordingLookupQuery())
+register_query(UserListensSessionQuery())
 
 app = create_app()
 load_config(app)
 init_sentry(app, "DATASETS_SENTRY_DSN")
+init_timescale_connection(app)
