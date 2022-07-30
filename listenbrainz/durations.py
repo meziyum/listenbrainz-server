62815726311556741fc0b4ec508965f67e4859c6
Kartik Ohri <kartikohri13@gmail.com>
2022-07-30 13:14:58 +0530

    fix with

--- listenbrainz/durations.py
+++ listenbrainz/durations.py
@@ -6,10 +6,10 @@ from listenbrainz.db import timescale as ts
 
 
 def create_length_cache_tmp():
-    with psycopg2.connect(current_app.config["MBID_MAPPING_DATABASE_URI"]) as mb_conn, \
-            mb_conn.cursor('recording_length_cursor', cursor_factory=DictCursor) as mb_curs, \
-            ts.engine.raw_connection() as lb_conn, \
-            lb_conn.cursor(cursor_factory=DictCursor) as lb_curs:
+    mb_conn = psycopg2.connect(current_app.config["MBID_MAPPING_DATABASE_URI"])
+    lb_conn = psycopg2.connect(current_app.config["SQLALCHEMY_TIMESCALE_URI"])
+    with mb_conn.cursor('recording_length_cursor', cursor_factory=DictCursor) as mb_curs, \
+         lb_conn.cursor(cursor_factory=DictCursor) as lb_curs:
         lb_curs.execute("CREATE TABLE mapping.recording_length_cache_tmp (gid UUID NOT NULL, length INT NOT NULL)")
         mb_curs.execute("SELECT gid, length FROM musicbrainz.recording WHERE length IS NOT NULL")
 
@@ -26,6 +26,6 @@ def create_length_cache_tmp():
             done += 1000
             print("Rows done: ", done)
 
-        lb_curs.execute(
-            "CREATE INDEX mapping.recording_length_cache_tmp_idx ON mapping.recording_length_cache_tmp(gid) INCLUDE (length)")
-        lb_conn.commit()
+        lb_curs.execute("CREATE INDEX mapping.recording_length_cache_tmp_idx"
+                        "          ON mapping.recording_length_cache_tmp(gid) INCLUDE (length)")
+    lb_conn.commit()
